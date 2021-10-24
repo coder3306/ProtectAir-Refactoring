2d6cca4cee311c69d08e7faec4aba7bc802c58b5
JeongSungGyu <84212041+coder3306@users.noreply.github.com>
2021-10-24 20:09:27 +0900

    Label 연결

--- ProtectAir/Cell/SecondRaspiCell.swift
+++ ProtectAir/Cell/SecondRaspiCell.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class SecondRaspiCell: UITableViewCell {
 
+    @IBOutlet weak var sTimeLabel: UILabel!
+    @IBOutlet weak var sNameLabel: UILabel!
+    @IBOutlet weak var sPm25Image: UIImageView!
+    @IBOutlet weak var sPm25Label: UILabel!
+    @IBOutlet weak var sPm100Image: UIImageView!
+    @IBOutlet weak var sPm100Label: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
     }
