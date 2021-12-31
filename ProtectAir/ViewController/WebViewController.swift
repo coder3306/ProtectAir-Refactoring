f4b4ab0558aee7a4b4976efead18204c7711ce6b
JeongSungGyu <84212041+coder3306@users.noreply.github.com>
2021-12-31 10:43:39 +0900

    web setup

--- ProtectAir/ViewController/WebViewController.swift
+++ ProtectAir/ViewController/WebViewController.swift
@@ -5,25 +5,60 @@
 //  Created by 정성규 on 2021/12/31.
 //
 
+import Foundation
 import UIKit
+import RxWebKit
+import RxCocoa
+import RxSwift
+import WebKit
 
 class WebViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var weatherButton: UIButton!
+    @IBOutlet weak var dustButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    private let bag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupWeb(){
+        let progressObservable = webView.rx.loading
+            .share()
+        
+        progressObservable
+            .map{ return $0 }
+            .observe(on: MainScheduler.instance)
+            .bind(to: progressBar.rx.isHidden)
+            .disposed(by: bag)
+        
+        progressObservable
+            .bind(to: UIApplication.shared.rx.isNetworkActivityIndicatorVisible)
+            .disposed(by: bag)
+        
+        webView.rx.title
+            .observe(on: MainScheduler.instance)
+            .bind(to: navigationItem.rx.title)
+            .disposed(by: bag)
+        
+        webView.rx.estimatedProgress
+            .map{ return Float($0) }
+            .observe(on: MainScheduler.instance)
+            .bind(to: progressBar.rx.progress)
+            .disposed(by: bag)
+        
+        weatherButton.rx.tap
+            .bind{
+                print("asdf")
+            }.disposed(by: bag)
+        
+        dustButton.rx.tap
+            .bind{
+                print("asdf")
+            }.disposed(by: bag)
+        
     }
-    */
-
 }
